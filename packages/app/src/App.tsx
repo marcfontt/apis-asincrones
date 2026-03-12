1ff79401e573ce681b4099fbca75715aa8b19082
Marc Font <marcfont3@gmail.com>
2026-03-12 10:16:15 +0000

    refactor: App.tsx net - 4 rutes principals Home/Cataleg/Escenaris/Resultats

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -1,63 +1,18 @@
 import { Navigate, Route } from 'react-router-dom';
-import { apiDocsPlugin, ApiExplorerPage } from '@backstage/plugin-api-docs';
-import {
-  CatalogEntityPage,
-  CatalogIndexPage,
-  catalogPlugin,
-} from '@backstage/plugin-catalog';
-import {
-  CatalogImportPage,
-  catalogImportPlugin,
-} from '@backstage/plugin-catalog-import';
-import { ScaffolderPage, scaffolderPlugin } from '@backstage/plugin-scaffolder';
-import { orgPlugin } from '@backstage/plugin-org';
-import { SearchPage } from '@backstage/plugin-search';
-import {
-  TechDocsIndexPage,
-  techdocsPlugin,
-  TechDocsReaderPage,
-} from '@backstage/plugin-techdocs';
-import { TechDocsAddons } from '@backstage/plugin-techdocs-react';
-import { ReportIssue } from '@backstage/plugin-techdocs-module-addons-contrib';
 import { UserSettingsPage } from '@backstage/plugin-user-settings';
-import { apis } from './apis';
-import { entityPage } from './components/catalog/EntityPage';
-import { searchPage } from './components/search/SearchPage';
-import { Root } from './components/Root';
-
-import {
-  AlertDisplay,
-  OAuthRequestDialog,
-  SignInPage,
-} from '@backstage/core-components';
+import { SearchPage } from '@backstage/plugin-search';
+import { AlertDisplay, OAuthRequestDialog, SignInPage } from '@backstage/core-components';
 import { createApp } from '@backstage/app-defaults';
 import { AppRouter, FlatRoutes } from '@backstage/core-app-api';
-import { CatalogGraphPage } from '@backstage/plugin-catalog-graph';
-import { RequirePermission } from '@backstage/plugin-permission-react';
-import { catalogEntityCreatePermission } from '@backstage/plugin-catalog-common/alpha';
 import { NotificationsPage } from '@backstage/plugin-notifications';
 import { SignalsDisplay } from '@backstage/plugin-signals';
-import { CatalogPage as AsyncCatalogPage, ScenariosPage as AsyncScenariosPage, RunsPage as AsyncRunsPage, MetricsPage as AsyncMetricsPage } from '../../../plugins/feina/src/plugin';
+import { apis } from './apis';
+import { searchPage } from './components/search/SearchPage';
+import { Root } from './components/Root';
+import { HomePage, CatalogPage, ScenariosPage, ResultatsPage } from '../../../plugins/feina/src/plugin';
 
 const app = createApp({
   apis,
-  bindRoutes({ bind }) {
-    bind(catalogPlugin.externalRoutes, {
-      createComponent: scaffolderPlugin.routes.root,
-      viewTechDoc: techdocsPlugin.routes.docRoot,
-      createFromTemplate: scaffolderPlugin.routes.selectedTemplate,
-    });
-    bind(apiDocsPlugin.externalRoutes, {
-      registerApi: catalogImportPlugin.routes.importPage,
-    });
-    bind(scaffolderPlugin.externalRoutes, {
-      registerComponent: catalogImportPlugin.routes.importPage,
-      viewTechDoc: techdocsPlugin.routes.docRoot,
-    });
-    bind(orgPlugin.externalRoutes, {
-      catalogIndex: catalogPlugin.routes.catalogIndex,
-    });
-  },
   components: {
     SignInPage: props => <SignInPage {...props} auto providers={['guest']} />,
   },
@@ -65,45 +20,14 @@ const app = createApp({
 
 const routes = (
   <FlatRoutes>
-    <Route path="/" element={<Navigate to="combinacions" />} />
-    <Route path="/catalog" element={<CatalogIndexPage />} />
-    <Route
-      path="/catalog/:namespace/:kind/:name"
-      element={<CatalogEntityPage />}
-    >
-      {entityPage}
-    </Route>
-    <Route path="/docs" element={<TechDocsIndexPage />} />
-    <Route
-      path="/docs/:namespace/:kind/:name/*"
-      element={<TechDocsReaderPage />}
-    >
-      <TechDocsAddons>
-        <ReportIssue />
-      </TechDocsAddons>
-    </Route>
-    <Route path="/create" element={<ScaffolderPage />} />
-    <Route path="/api-docs" element={<ApiExplorerPage />} />
-    <Route
-      path="/catalog-import"
-      element={
-        <RequirePermission permission={catalogEntityCreatePermission}>
-          <CatalogImportPage />
-        </RequirePermission>
-      }
-    />
-    <Route path="/search" element={<SearchPage />}>
-      {searchPage}
-    </Route>
-    <Route path="/settings" element={<UserSettingsPage />} />
-    <Route path="/catalog-graph" element={<CatalogGraphPage />} />
+    <Route path="/" element={<Navigate to="home" />} />
+    <Route path="/home"         element={<HomePage />} />
+    <Route path="/combinacions" element={<CatalogPage />} />
+    <Route path="/escenaris"    element={<ScenariosPage />} />
+    <Route path="/resultats"    element={<ResultatsPage />} />
+    <Route path="/search"       element={<SearchPage />}>{searchPage}</Route>
+    <Route path="/settings"     element={<UserSettingsPage />} />
     <Route path="/notifications" element={<NotificationsPage />} />
-    <Route path="/combinacions" element={<AsyncCatalogPage />} />
-    <Route path="/escenaris" element={<AsyncScenariosPage />} />
-    <Route path="/execucions" element={<AsyncRunsPage />} />
-    <Route path="/metriques" element={<AsyncMetricsPage />} />
-    <Route path="/execucions" element={<AsyncRunsPage />} />
-    <Route path="/metriques" element={<AsyncMetricsPage />} />
   </FlatRoutes>
 );
 
@@ -117,6 +41,3 @@ export default app.createRoot(
     </AppRouter>
   </>,
 );
-// FEINA plugin routes - afegir dins del <FlatRoutes>
-// <Route path="/feina-catalog" element={<CatalogPage />} />
-// <Route path="/feina-scenarios" element={<ScenariosPage />} />
