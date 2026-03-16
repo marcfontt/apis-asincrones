506665501d787d7d872658cb040d23eeb7810ea4
Marc Font <marcfont3@gmail.com>
2026-03-16 11:21:07 +0000

    refactor: rename sidebar Catàleg to /catalog route

--- packages/app/src/components/Root/Root.tsx
+++ packages/app/src/components/Root/Root.tsx
@@ -6,39 +6,16 @@ import ListAltIcon from '@material-ui/icons/ListAlt';
 import BarChartIcon from '@material-ui/icons/BarChart';
 import LogoFull from './LogoFull';
 import LogoIcon from './LogoIcon';
-import {
-  Settings as SidebarSettings,
-  UserSettingsSignInAvatar,
-} from '@backstage/plugin-user-settings';
+import { Settings as SidebarSettings, UserSettingsSignInAvatar } from '@backstage/plugin-user-settings';
 import { SidebarSearchModal } from '@backstage/plugin-search';
-import {
-  Sidebar,
-  sidebarConfig,
-  SidebarDivider,
-  SidebarGroup,
-  SidebarItem,
-  SidebarPage,
-  SidebarSpace,
-  useSidebarOpenState,
-  Link,
-} from '@backstage/core-components';
+import { Sidebar, sidebarConfig, SidebarDivider, SidebarGroup, SidebarItem, SidebarPage, SidebarSpace, useSidebarOpenState, Link } from '@backstage/core-components';
 import MenuIcon from '@material-ui/icons/Menu';
 import SearchIcon from '@material-ui/icons/Search';
 import { NotificationsSidebarItem } from '@backstage/plugin-notifications';
 
 const useSidebarLogoStyles = makeStyles({
-  root: {
-    width: sidebarConfig.drawerWidthClosed,
-    height: 3 * sidebarConfig.logoHeight,
-    display: 'flex',
-    flexFlow: 'row nowrap',
-    alignItems: 'center',
-    marginBottom: -14,
-  },
-  link: {
-    width: sidebarConfig.drawerWidthClosed,
-    marginLeft: 24,
-  },
+  root: { width: sidebarConfig.drawerWidthClosed, height: 3 * sidebarConfig.logoHeight, display: 'flex', flexFlow: 'row nowrap', alignItems: 'center', marginBottom: -14 },
+  link: { width: sidebarConfig.drawerWidthClosed, marginLeft: 24 },
 });
 
 const SidebarLogo = () => {
@@ -62,21 +39,17 @@ export const Root = ({ children }: PropsWithChildren<{}>) => (
       </SidebarGroup>
       <SidebarDivider />
       <SidebarGroup label="Menu" icon={<MenuIcon />}>
-        <SidebarItem icon={HomeIcon}    to="home"        text="Home" />
+        <SidebarItem icon={HomeIcon}     to="home"      text="Home" />
         <SidebarDivider />
-        <SidebarItem icon={StorageIcon} to="combinacions" text="Catàleg" />
-        <SidebarItem icon={ListAltIcon} to="escenaris"    text="Escenaris" />
-        <SidebarItem icon={BarChartIcon} to="resultats"   text="Resultats" />
+        <SidebarItem icon={StorageIcon}  to="catalog"   text="Catàleg" />
+        <SidebarItem icon={ListAltIcon}  to="escenaris" text="Escenaris" />
+        <SidebarItem icon={BarChartIcon} to="resultats" text="Resultats" />
       </SidebarGroup>
       <SidebarSpace />
       <SidebarDivider />
       <NotificationsSidebarItem />
       <SidebarDivider />
-      <SidebarGroup
-        label="Settings"
-        icon={<UserSettingsSignInAvatar />}
-        to="/settings"
-      >
+      <SidebarGroup label="Settings" icon={<UserSettingsSignInAvatar />} to="/settings">
         <SidebarSettings />
       </SidebarGroup>
     </Sidebar>
