9300b2a477719fe69f9793e692e70e5bfcb20364
Marc Font <marcfont3@gmail.com>
2026-03-16 11:21:06 +0000

    fix: add catalogPlugin to plugins array, fix settings apiRef error

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -6,6 +6,10 @@ import { createApp } from '@backstage/app-defaults';
 import { AppRouter, FlatRoutes } from '@backstage/core-app-api';
 import { NotificationsPage } from '@backstage/plugin-notifications';
 import { SignalsDisplay } from '@backstage/plugin-signals';
+import { catalogPlugin, CatalogIndexPage, CatalogEntityPage } from '@backstage/plugin-catalog';
+import { entityPage } from './components/catalog/EntityPage';
+import { scaffolderPlugin } from '@backstage/plugin-scaffolder';
+import { orgPlugin } from '@backstage/plugin-org';
 import { apis } from './apis';
 import { searchPage } from './components/search/SearchPage';
 import { Root } from './components/Root';
@@ -13,6 +17,17 @@ import { HomePage, CatalogPage, ScenariosPage, ResultatsPage } from '../../../pl
 
 const app = createApp({
   apis,
+  plugins: [catalogPlugin, scaffolderPlugin, orgPlugin],
+  bindRoutes({ bind }) {
+    bind(catalogPlugin.externalRoutes, {
+      createComponent: scaffolderPlugin.routes.root,
+      viewTechDoc: undefined as any,
+      createFromTemplate: scaffolderPlugin.routes.root,
+    });
+    bind(orgPlugin.externalRoutes, {
+      catalogIndex: catalogPlugin.routes.catalogIndex,
+    });
+  },
   components: {
     SignInPage: props => <SignInPage {...props} auto providers={['guest']} />,
   },
@@ -21,12 +36,17 @@ const app = createApp({
 const routes = (
   <FlatRoutes>
     <Route path="/" element={<Navigate to="home" />} />
-    <Route path="/home"         element={<HomePage />} />
-    <Route path="/combinacions" element={<CatalogPage />} />
-    <Route path="/escenaris"    element={<ScenariosPage />} />
-    <Route path="/resultats"    element={<ResultatsPage />} />
-    <Route path="/search"       element={<SearchPage />}>{searchPage}</Route>
-    <Route path="/settings"     element={<UserSettingsPage />} />
+    <Route path="/home"        element={<HomePage />} />
+    <Route path="/catalog"     element={<CatalogPage />} />
+    <Route path="/catalog/:id" element={<CatalogPage />} />
+    <Route path="/escenaris"   element={<ScenariosPage />} />
+    <Route path="/resultats"   element={<ResultatsPage />} />
+    <Route path="/bs-catalog"  element={<CatalogIndexPage />} />
+    <Route path="/bs-catalog/:namespace/:kind/:name" element={<CatalogEntityPage />}>
+      {entityPage}
+    </Route>
+    <Route path="/search"        element={<SearchPage />}>{searchPage}</Route>
+    <Route path="/settings"      element={<UserSettingsPage />} />
     <Route path="/notifications" element={<NotificationsPage />} />
   </FlatRoutes>
 );
